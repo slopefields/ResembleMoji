466d82b68963c6bf3f38665591bf8d5718e99303
slopefields <yooje44@gmail.com>
2025-02-03 19:06:09 -0800

    Add comments to script.js

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ const reader = new FileReader();
     
 imageUpload.addEventListener('change', function()
 {
+    // Get first file (only one image is allowed)
     const file = imageUpload.files[0];
+    // If null or undefined, exit function
     if (!file) return;
+    // Read file as a Data URL so it can be used as the source for the <img> tag
     reader.readAsDataURL(file);
 });
 
 reader.addEventListener("load", () =>
 {
+    // Change imageDisplay's src to the file data, or Data URL (only happens after readAsDataURL is loaded)
     imageDisplay.src = reader.result;
 });
 
 predictButton.addEventListener('click', function()
 {
+    // If there are no files (length == 0)
     if (!imageUpload.files.length)
         alert('Attach an image first!');
 });
