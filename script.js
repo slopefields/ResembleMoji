eacc605cbe7bbeaf11b73ead865b3c6a5f5b2c51
slopefields <yooje44@gmail.com>
2025-02-03 18:49:04 -0800

    Display image after it is attached

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const imageUpload = document.getElementById('imageUpload');
-
+const imageDisplay = document.getElementById('imageDisplay');
 const predictButton = document.getElementById('predictButton');
+
+const reader = new FileReader();
+    
+imageUpload.addEventListener('change', function()
+{
+    const file = imageUpload.files[0];
+    if (!file) return;
+    reader.readAsDataURL(file);
+});
+
+reader.addEventListener("load", () =>
+{
+    imageDisplay.src = reader.result;
+});
 
 predictButton.addEventListener('click', function()
 {
