4157359cd2872f46407f86135ad57c0202e44f7e
slopefields <yooje44@gmail.com>
2025-02-07 18:06:51 -0800

    Use @latest for MediaPipe FM and FD models

--- script.js
+++ script.js
@@ -1,104 +1,104 @@
-const imageUpload = document.getElementById('imageUpload');
-const imageDisplay = document.getElementById('imageDisplay');
-const predictButton = document.getElementById('predictButton');
+    const imageUpload = document.getElementById('imageUpload');
+    const imageDisplay = document.getElementById('imageDisplay');
+    const predictButton = document.getElementById('predictButton');
 
-const reader = new FileReader();
+    const reader = new FileReader();
 
-let mobileNetModel;
-let faceMeshModel;
-let faceDetectionModel;
+    let mobileNetModel;
+    let faceMeshModel;
+    let faceDetectionModel;
 
-function loadFaceDetectionModel()
-{
-    faceDetectionModel = new FaceDetection({locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection@0.0/${file}`;
-      }});
+    function loadFaceDetectionModel()
+    {
+        faceDetectionModel = new FaceDetection({locateFile: (file) => {
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/face_detection@latest/${file}`;
+        }});
 
-      faceDetectionModel.setOptions({
-        modelSelection: 0,
-        minDetectionConfidence: 0.5,
-      });
-    
-    console.log("MediaPipe Face Detection model successfully loaded");
-}
+        faceDetectionModel.setOptions({
+            modelSelection: 0,
+            minDetectionConfidence: 0.5,
+        });
+        
+        console.log("MediaPipe Face Detection model successfully loaded");
+    }
 
-function loadFaceMeshModel()
-{
-    faceMeshModel = new FaceMesh({locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
-      }});
+    function loadFaceMeshModel()
+    {
+        faceMeshModel = new FaceMesh({locateFile: (file) => {
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@latest/${file}`;
+        }});
 
-      faceMeshModel.setOptions({
-        maxNumFaces: 1,
-        refineLandmarks: false,
-        minDetectionConfidence: 0.5,
-      });
-      
-    console.log("MediaPipe Face Mesh model successfully loaded");
-}
+        faceMeshModel.setOptions({
+            maxNumFaces: 1,
+            refineLandmarks: false,
+            minDetectionConfidence: 0.5,
+        });
+        
+        console.log("MediaPipe Face Mesh model successfully loaded");
+    }
 
-async function loadMobileNetModel()
-{
-    mobileNetModel = await mobilenet.load();
-    console.log("MobileNet model successfully loaded");
-};
+    async function loadMobileNetModel()
+    {
+        mobileNetModel = await mobilenet.load();
+        console.log("MobileNet model successfully loaded");
+    };
 
-function displayPredictions(predictions)
-{
-    const predictionsDiv = document.getElementById('predictions');
-    predictionsDiv.innerHTML = "";
+    function displayPredictions(predictions)
+    {
+        const predictionsDiv = document.getElementById('predictions');
+        predictionsDiv.innerHTML = "";
 
-    predictions.forEach(prediction => {
-        const temp = document.createElement('p');
-        temp.textContent = `${prediction.className} : ${(prediction.probability * 100).toFixed(2)}%`;
-        predictionsDiv.appendChild(temp);
+        predictions.forEach(prediction => {
+            const temp = document.createElement('p');
+            temp.textContent = `${prediction.className} : ${(prediction.probability * 100).toFixed(2)}%`;
+            predictionsDiv.appendChild(temp);
+        });
+    }
+
+    async function predictUsingMobileNet()
+    {
+        const predictions = await mobileNetModel.classify(imageDisplay);
+            console.log(predictions);
+            displayPredictions(predictions);
+    }
+
+    imageUpload.addEventListener('change', function()
+    {
+        // Get first file (only one image is allowed)
+        const file = imageUpload.files[0];
+        // If null or undefined, exit function
+        if (!file) return;
+        // Read file as a Data URL so it can be used as the source for the <img> tag
+        reader.readAsDataURL(file);
     });
-}
 
-async function predictUsingMobileNet()
-{
-    const predictions = await mobileNetModel.classify(imageDisplay);
-        console.log(predictions);
-        displayPredictions(predictions);
-}
-
-imageUpload.addEventListener('change', function()
-{
-    // Get first file (only one image is allowed)
-    const file = imageUpload.files[0];
-    // If null or undefined, exit function
-    if (!file) return;
-    // Read file as a Data URL so it can be used as the source for the <img> tag
-    reader.readAsDataURL(file);
-});
-
-reader.addEventListener("load", () =>
-{
-    // Change imageDisplay's src to the file data, or Data URL (only happens after readAsDataURL is loaded)
-    imageDisplay.src = reader.result;
-});
-
-predictButton.addEventListener('click', async function()
-{
-    // If there are no files (length == 0)
-    if (!imageUpload.files.length)
+    reader.addEventListener("load", () =>
     {
-        alert('Attach an image first!');
-    }
-    else if (!mobileNetModel)
-    {
-        alert('MobileNet model still loading. Try again later.');
-    }
-    else
-    {
-        predictUsingMobileNet();
-    }
-    
-});
+        // Change imageDisplay's src to the file data, or Data URL (only happens after readAsDataURL is loaded)
+        imageDisplay.src = reader.result;
+    });
 
-document.addEventListener("DOMContentLoaded", () =>
-{
-    loadFaceDetectionModel();
-    loadFaceMeshModel();
-    loadMobileNetModel();
-});
+    predictButton.addEventListener('click', async function()
+    {
+        // If there are no files (length == 0)
+        if (!imageUpload.files.length)
+        {
+            alert('Attach an image first!');
+        }
+        else if (!mobileNetModel)
+        {
+            alert('MobileNet model still loading. Try again later.');
+        }
+        else
+        {
+            predictUsingMobileNet();
+        }
+        
+    });
+
+    document.addEventListener("DOMContentLoaded", () =>
+    {
+        loadFaceDetectionModel();
+        loadFaceMeshModel();
+        loadMobileNetModel();
+    });
