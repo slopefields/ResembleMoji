938a8c7481a89398303269919ada7db540a9338d
slopefields <yooje44@gmail.com>
2025-03-06 16:58:29 -0800

    Add game levels and progressLevel function

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,8 @@
 import { ExpressionModel } from './expression_detection.js';
 import { ObjectModel } from './object_detection.js';
 import { Camera, VIDEO_PIXELS } from './camera.js';
-import { displayExpressionPredictions, displayObjectPredictions, clearResults } from './script.js';
+import { displayExpressionPredictions, displayObjectPredictions, clearResults, shuffleArray } from './script.js';
+import { EMOJIS_LVL_1, EMOJIS_LVL_2, EMOJIS_LVL_3, EMOJIS_LVL_4, EMOJIS_LVL_5 } from './game_levels.js';
 
 export class Game
 {
@@ -10,7 +11,19 @@ export class Game
     expressionModel;
     objectModel;
     camera;
+    
+    level1;
+    level2;
+    level3;
+    level4;
+    level5;
+    levelLookup;
 
+    gameDifficulty;
+    currentDifficulty;
+    currentLevelIndex;
+
+    currentEmoji;
 
     constructor()
     {
@@ -19,7 +32,36 @@ export class Game
         this.objectModel = new ObjectModel();
         this.expressionModel = new ExpressionModel();
         this.camera = new Camera();
+
+        this.level1 = shuffleArray(EMOJIS_LVL_1);
+        this.level2 = shuffleArray(EMOJIS_LVL_2);
+        this.level3 = shuffleArray(EMOJIS_LVL_3);
+        this.level4 = shuffleArray(EMOJIS_LVL_4);
+        this.level5 = shuffleArray(EMOJIS_LVL_5);
+
+        // 12 levels total
+        this.gameDifficulty = '112122232345';
+        this.currentLevelIndex = 0;
+        this.currentDifficulty = this.gameDifficulty[this.currentLevelIndex];
+
+        this.levelLookup = 
+        {
+            '1': this.level1,
+            '2': this.level2,
+            '3': this.level3,
+            '4': this.level4,
+            '5': this.level5
+        }
+
+        this.currentEmoji = this.levelLookup[this.currentDifficulty].shift();
+        console.log("CURRENT EMOJI: ", this.currentEmoji);
     }
+
+    async loadModels() {
+        await this.expressionModel.loadFaceExpressionModel();
+        await this.objectModel.load();
+    }
+
 
     async makePrediction()
     {
@@ -78,39 +120,33 @@ export class Game
         requestAnimationFrame(() => this.makePrediction());
     }
 
-    async loadModels() {
-        await this.expressionModel.loadFaceExpressionModel();
-        await this.objectModel.load();
-    }
-
-    async startGame()
+    async prepareCamera()
     {
         if (this.camera.checkWebcamSupport()) {
-            console.log("Webcam support valid");
             await this.camera.enableWebcam();
             
             // Wait until the video is actually loaded
             if (this.camera.videoElement.readyState >= 2) {
-                console.log("Video is loaded, starting predictions...");
-                this.makePrediction();
+                console.log("Video is loaded, starting game...");
+                this.startGame();
             } else {
                 console.error("Error: Video element is not ready.");
             }
         } else {
             console.log("Webcam support invalid");
         }
+    }
 
-        /* Old
-        if (this.camera.checkWebcamSupport())
-        {
-            console.log("Webcam support valid");
-            await this.camera.enableWebcam();
-            this.makePrediction();
-        }
-        else
-        {
-            console.log("Webcam support invalid");
-        }
-        */
+    startGame()
+    {
+        this.makePrediction();
+    }
+
+    progressLevel()
+    {
+        this.currentLevelIndex++;
+        this.currentDifficulty = this.gameDifficulty[this.currentLevelIndex];
+        this.currentEmoji = this.levelLookup[this.currentDifficulty].shift();
+        console.log("Current emoji: ", this.currentEmoji);
     }
 }
